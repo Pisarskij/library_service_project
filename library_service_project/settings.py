25fea9ed44c5d69a45ab2fde58e5e926ecb93f44
soobig666 <darkvader.ukr.net@gmail.com>
2023-06-23 14:34:11 +0300

    add custom permissions & set-up it to settings

--- library_service_project/settings.py
+++ library_service_project/settings.py
@@ -144,9 +144,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    # "DEFAULT_PERMISSION_CLASSES": [
-    #     "user.permissions.IsAdminOrIfAuthenticatedReadOnly",
-    # ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "user.permissions.IsAdminOrIfAuthenticatedReadOnly",
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
