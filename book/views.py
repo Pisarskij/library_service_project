329e27a160b584ffe59d23e9bc9e83c302ad63c5
soobig666 <darkvader.ukr.net@gmail.com>
2023-06-23 16:14:33 +0300

    add Book model && add Book urls to main urls.py

--- book/views.py
+++ book/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
