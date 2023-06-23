ced3435996601e260b2c8908b87de550478115f8
soobig666 <darkvader.ukr.net@gmail.com>
2023-06-23 15:29:59 +0300

    upd urls name && add documentations urls

--- library_service_project/urls.py
+++ library_service_project/urls.py
@@ -26,17 +26,17 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/user/", include("user.urls", namespace="user")),
-    # path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    # path(
-    #     "api/doc/swagger/",
-    #     SpectacularSwaggerView.as_view(url_name="schema"),
-    #     name="swagger-ui",
-    # ),
-    # path(
-    #     "api/doc/redoc/",
-    #     SpectacularRedocView.as_view(url_name="schema"),
-    #     name="redoc",
-    # ),
+    path("api/users/", include("user.urls", namespace="user")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
