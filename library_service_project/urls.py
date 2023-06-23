326fdf718caa9797d0fe11c175c6b93a97d3487a
soobig666 <darkvader.ukr.net@gmail.com>
2023-06-23 14:31:12 +0300

    add custom model User

--- library_service_project/urls.py
+++ library_service_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for library_service_project project.
+URL configuration for social_media_api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,9 +14,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("api/user/", include("user.urls", namespace="user")),
+    # path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # path(
+    #     "api/doc/swagger/",
+    #     SpectacularSwaggerView.as_view(url_name="schema"),
+    #     name="swagger-ui",
+    # ),
+    # path(
+    #     "api/doc/redoc/",
+    #     SpectacularRedocView.as_view(url_name="schema"),
+    #     name="redoc",
+    # ),
+    path("__debug__/", include("debug_toolbar.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user/views.py
+++ user/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from user.models import User
+from user.serializers import UserSerializer, UserCreateSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserCreateSerializer
+    authentication_classes = ()
+    permission_classes = ()
+
+
+class UserView(
+    mixins.UpdateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class SelfUserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+    queryset = User.objects
+
+    def get_object(self):
+        return self.request.user
