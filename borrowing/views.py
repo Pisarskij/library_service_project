1acd28f0a6b724f41fb6b9e838676f0abe4ca86f
soobig666 <darkvader.ukr.net@gmail.com>
2023-06-27 13:34:01 +0300

    add filtering by user_id & actual_return_date

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+from django.utils.functional import lazy
 from rest_framework import viewsets, mixins, serializers
 
 from rest_framework.permissions import IsAuthenticated
@@ -16,9 +19,30 @@ class BorrowingViewSet(
 
     def get_queryset(self):
         user = self.request.user
+        user_id = self.request.query_params.get("user_id", None)
+        is_active_str = self.request.query_params.get("is_active", None)
+        """
+        Return a queryset for the current user or user filtering by user_id in url
+        ONLY for is_staff=true
+        """
         if user.is_staff:
-            return Borrowing.objects.all()
-        return Borrowing.objects.filter(user_id=user.id)
+            queryset = (
+                Borrowing.objects.filter(user_id=user_id)
+                if user_id
+                else Borrowing.objects.all()
+            )
+        # If is_staff=false will return only user-own borrowing
+        else:
+            queryset = Borrowing.objects.filter(user_id=user.id)
+        """
+        Return a queryset for is_active_str in url (is_active=True, return all datas with actual_return_date = null)
+        ONLY for is_staff=false
+        """
+        if is_active_str is not None:
+            is_active = is_active_str.lower() == "true"
+            queryset = queryset.filter(actual_return_date__isnull=is_active)
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action in ["retrieve", "update", "partial_update"]:
