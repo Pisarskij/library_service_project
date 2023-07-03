7314523c1a92a86d93b88cea6c069f793500fbc1
soobig666 <darkvader.ukr.net@gmail.com>
2023-07-03 22:08:51 +0300

    fix create_stripe_session ( set correct success_url & set "metadata"

--- payment/session.py
+++ payment/session.py
@@ -1,29 +1,22 @@
-from typing import Any
-
 import stripe
 from django.http import JsonResponse
 
+from borrowing.models import Borrowing
 from library_service_project import settings
 
 stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
 
 
 def create_stripe_session(
-    book,
     money_to_pay,
-    borrowing=None,
     payment=None,
     session=None,
 ):
     check_session = check_stripe_session(session)
-
     if not check_session or session is None:
+        borrowing = Borrowing.objects.get(payments=payment)
         session = stripe.checkout.Session.create(
-            metadata={
-                "borrowing_id": borrowing,
-                "payment_id": borrowing,
-            },
-            success_url="http://127.0.0.1:8000/success/",
+            success_url=f"http://127.0.0.1:8000/api/borrowing/{borrowing.id}/",
             cancel_url="http://127.0.0.1:8000/api/",
             payment_method_types=["card"],
             line_items=[
@@ -31,7 +24,7 @@ def create_stripe_session(
                     "price_data": {
                         "currency": "usd",
                         "product_data": {
-                            "name": str(book.title),
+                            "name": f"Invoice for payment №{payment}",
                         },
                         "unit_amount": money_to_pay,
                     },
@@ -39,6 +32,7 @@ def create_stripe_session(
                 },
             ],
             mode="payment",
+            metadata={"payment_id": payment},
         )
         return session
 
