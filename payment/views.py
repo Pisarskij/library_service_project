8029baa23958babddd743d5d3a902ef1c08d3488
soobig666 <darkvader.ukr.net@gmail.com>
2023-07-04 15:21:22 +0300

    upd with get_object in Payment

--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework import mixins
 
 from payment.models import Payment
 from payment.serializers import PaymentSerializer
+from payment.session import check_stripe_session
 
 
 class PaymentViewSet(
@@ -13,3 +15,9 @@ class PaymentViewSet(
 ):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+
+    def get_object(self):
+        payment_pk = self.kwargs["pk"]
+        payment = get_object_or_404(Payment, pk=payment_pk)
+        check_stripe_session(payment_pk)
+        return payment
